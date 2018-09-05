a33e5d0c4379b3ee945ab7072de89dbaaaa800cd
朱帮乾 <summer.zhu@ximalaya.com>
2018-09-05 18:44:08 +0800

    Initial Commit

---
+++ RevertJsonKeyPath/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RevertJsonKeyPath
+//
+//  Created by zhu bangqian on 2018/9/5.
+//  Copyright © 2018年 zhu bangqian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RevertJsonKeyPath/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RevertJsonKeyPath
+//
+//  Created by zhu bangqian on 2018/9/5.
+//  Copyright © 2018年 zhu bangqian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
